3e55b3c9ca9566653148eb5965469e1305f7e3ba
BM-ACADEMY <teamkamarweb@gmail.com>
2025-09-09 18:08:46 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import Navbar from "./Modules/Layout/Navbar";
 import AppRoutes from "./Routes/Routes"; // make sure the path is correct
 import Footer from "./Modules/Layout/Footer";
-import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import { Scrolltotop } from "./scrolltop/Scrolltotop";
+import { AuthProvider } from "./Context/Authcontext";
 
 function AppContent() {
   const location = useLocation();
